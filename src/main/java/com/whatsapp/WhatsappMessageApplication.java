2c72ad5de5121dbbd59b9c4cf58f5e49f49caa9a
ashutosh44-star <ashutoshchauhan149@gmail.com>
2024-06-23 11:25:00 +0530

    A SpringBoot Project for Direct Whatsapp Message

---
+++ src/main/java/com/whatsapp/WhatsappMessageApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappMessageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/whatsapp/WhatsappMessageSend/WhatsappMessageSend.java
@@ -0,0 +1,37 @@
+package com.whatsapp.WhatsappMessageSend;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Controller
+public class WhatsappMessageSend {
+
+	Integer cp; 
+	Integer id;
+	String img="";
+
+	@GetMapping("/main")
+	public String dashboard(Model m, Principal principal) {
+	     return "main";
+	}
+	
+	@PostMapping("/sendMessage")
+	public String sendMessage(Model m, @RequestParam("mobileNo") String mobileNo,
+			HttpServletResponse res) throws IOException {
+		System.out.println("inside sendMessage--------"+mobileNo);
+	    res.sendRedirect("https://wa.me/"+ mobileNo);
+		return "main";
+	}
+}
